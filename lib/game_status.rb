980f256913d4c1d782e16fb0d0ccc8d42b15c14b
Sophie Gu <sophieqgu@gmail.com>
2018-08-21 02:56:00 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,7 +25,7 @@ WIN_COMBINATIONS = [
       position_2 = board[win_index_2]
       position_3 = board[win_index_3]
       
-      if position_1 == position_2 && position_2 == position_3 && [position_1, position_2, position_3].all?{|i| position_taken?(board, i)}
+      if position_1 == position_2 && position_2 == position_3 && [position_1, position_2, position_3].any?{|i| position_taken?(board, i)}
         return win_combination 
       else
         false
